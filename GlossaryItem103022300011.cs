2ee9eb735be24688a830975bb978909f0d1fb37c
Lenovo <frizammaulana07@gmail.com>
2025-04-09 17:19:59 +0700

    menambahkan class GlossaryItem103022300011

---
+++ GlossaryItem103022300011.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace jurnalmodul7_kelompok_7
+{
+    public class Glossary {
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+    }
+
+    public class GlossDiv
+    {
+        [JsonPropertyName("title")]
+        public string TitleDiv { get; set; }
+    }
+
+    public class GlossDef {
+        [JsonPropertyName("para")]
+        public string Para { get; set; }
+        
+        [JsonPropertyName("GlossSeeAlso")]
+        public string GlossSeeAlso { get; set; }
+
+    }
+
+    public class GlossEntry
+    {
+
+    }
+
+    class GlossaryItem103022300011
+    {
+    }
+}
